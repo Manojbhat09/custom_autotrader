f3bc5769349b9953b9f805abf7da97eb966e3960
manojbhat09 <manojbhat09@gmail.com>
2023-11-19 00:00:08 -0800

    add sd

---
+++ sdsp/clipseg/install.bash
@@ -0,0 +1,10 @@
+
+# git lfs install
+
+# git clone https://github.com/timojl/clipseg 
+
+pip install diffusers -q
+
+pip install transformers -q -UU ftfy gradio  
+
+pip install git+https://github.com/openai/CLIP.git -q
